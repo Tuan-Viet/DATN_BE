39008239b08bf1a6a96fb482eb8c8e470bc1af05
thanhvt1509 <92007722+thanhvt1509@users.noreply.github.com>
2023-10-12 02:22:08 +0700

    api color product

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,6 +3,7 @@ import routerProduct from "./product"
 import routerCategory from "./category.js"
 import routerImages from "./upload.js"
 import routerSize from "./size"
+import routerColor from "./color"
 
 const router = express.Router()
 
@@ -10,6 +11,7 @@ router.use('/products', routerProduct)
 router.use('/categories', routerCategory)
 router.use('/images', routerImages)
 router.use('/sizes', routerSize)
+router.use('/colors', routerColor)
 
 
 export default router
