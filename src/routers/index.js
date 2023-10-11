65cf1b0e2929d6122fe15815d87488b5a4ca908b
thanhvt1509 <92007722+thanhvt1509@users.noreply.github.com>
2023-10-12 02:06:02 +0700

    api size product

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,12 +2,14 @@ import express from "express"
 import routerProduct from "./product"
 import routerCategory from "./category.js"
 import routerImages from "./upload.js"
+import routerSize from "./size"
 
 const router = express.Router()
 
 router.use('/products', routerProduct)
 router.use('/categories', routerCategory)
 router.use('/images', routerImages)
+router.use('/sizes', routerSize)
 
 
 export default router
