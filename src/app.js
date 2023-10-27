095fede7e0c2d0b4eed98ea14a08b7f6931f6fcc
TuanViet <vietlt226688@gmail.com>
2023-10-27 23:48:52 +0700

    update merge dev

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: "GET,POST,PUT,DELETE",
+    methods: "GET,POST,PUT,DELETE,PATCH",
     ocredentials: true,
   })
 );
